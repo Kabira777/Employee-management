030a8201e9a6aa90f92c233d6a6e84006438ca7b
Kabira777 <dixitpranshu336@gmail.com>
2025-07-26 04:11:12 +0530

    login page created

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+loginObj:any={
+   username:'',
+   password:'',
+}
 
+constructor(private router:Router){}
+
+onLogin(){
+  if(this.loginObj.username.trim()==''|| this.loginObj.password.trim()==''){
+    alert('Please enter your credentials')
+  }
+  else{
+     if(this.loginObj.username=="admin" && this.loginObj.password=="admin786"){
+
+      this.router.navigate(['/dashboard']);
+      alert("Kabiraaaa")
+      }
+       else{
+      alert("Wrong credentials");
+        }
+     }
+ 
+
+}
 }
